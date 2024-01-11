a80eb5287296653e7629ce80f99b1b63c356b865
Steven Mentzer <stevenfmentzer@gmail.com>
2024-01-10 21:00:08 -0500

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+
+        self.title = title
+        self.page_count = page_count
     
-        
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title_name):
+        self._title = title_name
+
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else: 
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
